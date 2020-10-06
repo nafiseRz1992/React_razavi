6ac79db14a2be2444224589b0757bf13897504be
nafiseRz1992 <razavi.nafise@gmail.com>
2020-10-06 23:12:01 +0330

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Album from "./Album";
+
+
+const App =()=>{
+
+    return <div>
+        <Album/>
+    </div>
+};
+
+export default App;
+
---
+++ src/Post.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Post = ()=>{
+    return <div>
+        <h3>Post Title</h3>
+        <p>1. today is good today is good today is good today is good today is good today is good today is good
+        </p>
+
+        <h3>Post Title</h3>
+        <p>2. today is good today is good today is good today is good today is good today is good today is good
+        </p>
+
+        <h3>Post Title</h3>
+        <p>3. today is good today is good today is good today is good today is good today is good today is good
+        </p>
+
+        <h3>Post Title</h3>
+        <p>4. today is good today is good today is good today is good today is good today is good today is good
+        </p>
+
+        <h3>Post Title</h3>
+        <p>5. today is good today is good today is good today is good today is good today is good today is good
+        </p>
+
+    </div>
+}
+
+export  default Post;
